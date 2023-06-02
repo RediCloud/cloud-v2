08014cd4b14bde53c2f078454844fbf671866625
Suqatri <paul@suqatri.net>
2023-06-02 17:19:49 +0200

    feat: database config from/to file/env

--- database/src/main/kotlin/dev/redicloud/database/config/DatabaseConfig.kt
+++ database/src/main/kotlin/dev/redicloud/database/config/DatabaseConfig.kt
@@ -1,9 +1,41 @@
 package dev.redicloud.database.config
 
+import dev.redicloud.utils.prettyPrintGson
+import java.io.File
+
 data class DatabaseConfig(
     val password: String = "",
     val nodes: List<DatabaseNode>,
     val databaseId: Int = 0
 ) {
+
     fun isCluster(): Boolean = nodes.size > 1
+
+    companion object {
+        fun fromEnv(): DatabaseConfig {
+            val password = System.getenv("RC_DATABASE_PASSWORD") ?: ""
+            val databaseId = System.getenv("RC_DATABASE_ID")?.toInt() ?: 0
+            val nodes = System.getenv("RC_DATABASE_NODES")?.split(";")?.map {
+                val split = it.split(":")
+                DatabaseNode(split[0], split[1].toInt())
+            } ?: listOf(DatabaseNode("127.0.0.1", 6379))
+            return DatabaseConfig(password, nodes, databaseId)
+        }
+
+        fun fromFile(file: File): DatabaseConfig {
+            return prettyPrintGson.fromJson(file.readText(Charsets.UTF_8), DatabaseConfig::class.java)
+        }
+
+    }
+
+}
+
+fun DatabaseConfig.toEnv(processBuilder: ProcessBuilder) {
+    processBuilder.environment()["RC_DATABASE_PASSWORD"] = password
+    processBuilder.environment()["RC_DATABASE_ID"] = databaseId.toString()
+    processBuilder.environment()["RC_DATABASE_NODES"] = nodes.joinToString(";") { "${it.hostname}:${it.port}" }
+}
+
+fun DatabaseConfig.toFile(file: File) {
+    file.writeText(prettyPrintGson.toJson(this), Charsets.UTF_8)
 }
