a0d3fbf2ed3b13c6a0370ffac5b64122138762c9
Suqatri <paul@suqatri.net>
2023-06-02 17:24:36 +0200

    use command responses

---
+++ commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandResponse.kt
@@ -0,0 +1,12 @@
+package dev.redicloud.commands.api
+
+data class CommandResponse(val type: CommandResponseType, val message: String? = null, val exception: Exception? = null)
+
+enum class CommandResponseType {
+    SUCCESS,
+    ERROR,
+    INVALID_COMMAND,
+    INVALID_ARGUMENT_TYPE,
+    INVALID_ARGUMENT_COUNT,
+    INVALID_SUB_PATH
+}
