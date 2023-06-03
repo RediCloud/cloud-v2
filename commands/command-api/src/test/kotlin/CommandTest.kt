c4301ab2183ed9b4e40e728b297b0e7afd0b61cf
Suqatri <paul@suqatri.net>
2023-06-03 12:58:15 +0200

    test: add some tests

--- commands/command-api/src/test/kotlin/CommandTest.kt
+++ commands/command-api/src/test/kotlin/CommandTest.kt
@@ -3,32 +3,39 @@ import dev.redicloud.commands.api.*
 fun main() {
     val commandManager = CommandManager()
     commandManager.register(TestCommand())
+    
+    /*
     commandManager.getCommands().forEach {
-        println("Command: ${it.getName()}")
-        println("Description: ${it.getDescription()}")
-        println("Aliases: ${it.getAliases().joinToString(", ")}")
-        it.getSubCommands().forEach { sub ->
-            println("Sub command: ${sub.path}")
-            println("\tDescription: ${sub.description}")
-            println("\tAliases: ${sub.aliasePaths.joinToString(", ")}")
-            sub.arguments.forEach { arg ->
-                println("\tArgument: ${arg.name}")
-                println("\t\tRequired: ${arg.required}")
-                println("\t\tType: ${arg.clazz.qualifiedName}")
-            }
+    println("Command: ${it.getName()}")
+    println("Description: ${it.getDescription()}")
+    println("Aliases: ${it.getAliases().joinToString(", ")}")
+    it.getSubCommands().forEach { sub ->
+        println("Sub command: ${sub.path}")
+        println("\tDescription: ${sub.description}")
+        println("\tAliases: ${sub.aliasePaths.joinToString(", ")}")
+        sub.arguments.forEach { arg ->
+            println("\tArgument: ${arg.name}")
+            println("\t\tRequired: ${arg.required}")
+            println("\t\tType: ${arg.clazz.qualifiedName}")
         }
-        /*
-        println("Path without args:")
-        it.getPathsWithArguments().forEach { path ->
-            println("\t$path")
-        }
-         */
+    }
+    println("Path without args:")
+    it.getPathsWithArguments().forEach { path ->
+        println("\t$path")
+    }
         println("Paths with args:")
         it.getPaths().forEach { path ->
             println("\t$path")
         }
         println("")
     }
+     */
+
+
+    val response1 = commandManager.handleInput("test sub1 secondsub1 Ein")
+    val response2 = commandManager.handleInput("test sub1 ss2 Elias")
+    println(response1)
+    println(response2)
 }
 
 
@@ -37,13 +44,27 @@ fun main() {
 class TestCommand() : CommandBase() {
 
     @CommandSubPath("sub1 secondsub1")
-    @CommandAlias(["sub1 secondsub2", "sub1 secondsub3"])
+    @CommandAlias(["sub1 ss1"])
     fun sub1(
         @CommandParameter(name = "message1", required = true) message1: String?,
         @CommandParameter(name = "message2", required = false) message2: String?,
         @CommandParameter(name = "message3", required = false) message3: String?
     ) {
-        println("Sub1")
+        println("S1: Message1: $message1")
+        println("S1: Message2: $message2")
+        println("S1: Message3: $message3")
+    }
+
+    @CommandSubPath("sub1 secondsub2")
+    @CommandAlias(["sub1 ss2"])
+    fun sub2(
+        @CommandParameter(name = "message1", required = true) message1: String?,
+        @CommandParameter(name = "message2", required = false) message2: String?,
+        @CommandParameter(name = "message3", required = false) message3: String?
+    ) {
+        println("S2: Message1: $message1")
+        println("S2: Message2: $message2")
+        println("S2: Message3: $message3")
     }
 
 }
