ef22fd5c6268309af9cd99967439ec9006cb74f0
Suqatri <paul@suqatri.net>
2023-06-03 12:57:06 +0200

    fix: handleInput

--- commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandManager.kt
+++ commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandManager.kt
@@ -13,16 +13,43 @@ open class CommandManager {
     }
 
     fun handleInput(input: String): CommandResponse {
+        if (input.isBlank()) return CommandResponse(CommandResponseType.INVALID_COMMAND, "Command cannot be blank")
         val split = input.split(" ")
         val commandName = split[0].lowercase()
+        val parameters = split.drop(1)
         val command = commands.firstOrNull { it.getName().lowercase() == commandName }
             ?: return CommandResponse(CommandResponseType.INVALID_COMMAND, "Command $commandName not found")
-        val subCommand = command.getSubCommands().firstOrNull { it.getSubPathsWithoutArguments().contains(split.joinToString(" ")) }
-            ?: return CommandResponse(CommandResponseType.INVALID_SUB_PATH, "Sub command ${split.joinToString(" ")} not found")
-        val possiblePaths = subCommand.getSubPaths()
-        val path = possiblePaths.firstOrNull { it == split.joinToString(" ") }
-            ?: return CommandResponse(CommandResponseType.INVALID_SUB_PATH, "Path ${split.joinToString(" ")} not found")
-        return CommandResponse(CommandResponseType.SUCCESS, "Path $path found")
+
+        val possibleFullPaths = command.getPaths()
+        val possiblePaths = command.getPathsWithArguments()
+
+        val matched = possibleFullPaths.toMutableList()
+        var index = -1
+        parameters.forEach {
+            index++
+            val possible = matched.filter { path ->
+                val parameterSplit = path.split(" ")
+                if (parameterSplit.size <= index) return@filter false
+                val parameter = parameterSplit[index].lowercase()
+                parameter == it.lowercase() || parameter.isOptionalArgument() || parameter.isRequiredArgument()
+            }
+            matched.clear()
+            matched.addAll(possible)
+        }
+
+        val subCommand = command.getSubCommands()
+            .firstOrNull { subCommand -> subCommand.getSubPaths()
+                .any { it -> it.lowercase() == matched.first().lowercase() } }
+            ?: return CommandResponse(CommandResponseType.INVALID_SUB_PATH, "Invalid sub path for command $commandName ${parameters.joinToString(" ")}")
+        val optimalPath = subCommand.getSubPaths().firstOrNull { it.lowercase() == matched.first().lowercase() }!!
+        var pathWithoutArgs = ""
+        optimalPath.split(" ").forEach {
+            if (it.isOptionalArgument() || it.isRequiredArgument()) return@forEach
+            if (pathWithoutArgs.isNotBlank()) pathWithoutArgs += " "
+            pathWithoutArgs += it
+        }
+        val arguments = parameters.drop(pathWithoutArgs.split(" ").size)
+        return subCommand.execute(arguments)
     }
 
     fun getCommands(): List<CommandBase> = commands.toList()
