0c13672f934186297c22c1a60c26d7e81f93bdc0
Suqatri <paul@suqatri.net>
2023-06-04 14:42:14 +0200

    fix: use isThis to identify argument/command/subcommand

--- commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandArgument.kt
+++ commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandArgument.kt
@@ -4,14 +4,15 @@ import java.lang.reflect.Parameter
 import kotlin.reflect.KClass
 import kotlin.reflect.full.superclasses
 
-class CommandArgument(commandSubBase: CommandSubBase, parameter: Parameter, val index: Int) {
+class CommandArgument(val subCommand: CommandSubBase, parameter: Parameter, val index: Int) {
 
     val name: String
     val required: Boolean //TODO
     val clazz: KClass<*>
     val parser: CommandArgumentParser<*>?
     val actorArgument: Boolean
-    val suggester: ICommandSuggester
+    val annotatedSuggester: ICommandSuggester
+    val suggester: CommandArgumentSuggester
 
     init {
         if (parameter.type.kotlin.superclasses.any { it == ICommandActor::class }) {
@@ -20,24 +21,39 @@ class CommandArgument(commandSubBase: CommandSubBase, parameter: Parameter, val 
             clazz = parameter.type.kotlin
             parser = null
             actorArgument = true
-            suggester = EmptySuggester()
+            annotatedSuggester = EmptySuggester()
         }else {
             actorArgument = false
             if (!parameter.isAnnotationPresent(CommandParameter::class.java)) {
                 name = parameter.name
                 required = !parameter.isImplicit //TODO check String? and Int? etc.
-                suggester = ICommandSuggester.SUGGESTERS.firstOrNull { it::class == parameter.type.kotlin } ?: EmptySuggester()
+                annotatedSuggester = ICommandSuggester.SUGGESTERS.firstOrNull { it::class == parameter.type.kotlin } ?: EmptySuggester()
             } else {
                 val annotation = parameter.getAnnotation(CommandParameter::class.java)
                 name = annotation.name.ifEmpty { parameter.name }
                 required = annotation.required //TODO check String? and Int? etc.
-                suggester = EmptySuggester()
+                annotatedSuggester = EmptySuggester()
             }
             clazz = parameter.type.kotlin
             parser = CommandArgumentParser.PARSERS.filter {
                 it.key.qualifiedName!!.replace("?", "") == clazz.qualifiedName!!.replace("?", "")
             }.values.first()
         }
+        suggester = CommandArgumentSuggester(this)
+    }
+
+    fun isThis(input: String, predict: Boolean): Boolean {
+        if (!subCommand.isThis(input, false)) return false
+        if (actorArgument) return false
+        if (input.isEmpty()) return false
+        var argumentPath = input.lowercase()
+        subCommand.getSubPathsWithoutArguments().forEach {
+            argumentPath = argumentPath.replace(it, "")
+        }
+        val argumentSplit = argumentPath.split(" ")
+        if (argumentSplit.size <= index) return false
+        if (argumentSplit.size == index + 1 && !argumentPath.endsWith(" ")) return true
+        return argumentSplit.size == index && argumentPath.endsWith(" ") && predict
     }
 
     fun parse(input: String): Any? = parser?.parse(input)
