99a93e4e99e37ca3058a4bea5adeb31542bec8d7
Suqatri <paul@suqatri.net>
2023-06-04 13:03:42 +0200

    feat: add sendMessage function to actor

--- commands/command-api/src/main/kotlin/dev/redicloud/commands/api/ICommandActor.kt
+++ commands/command-api/src/main/kotlin/dev/redicloud/commands/api/ICommandActor.kt
@@ -3,4 +3,7 @@ package dev.redicloud.commands.api
 interface ICommandActor<T> {
     val identifier: T
     fun hasPermission(permission: String?): Boolean
+
+    fun sendMessage(message: String)
+
 }
