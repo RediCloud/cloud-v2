d3e5f079cc01b9545e39a2e389b7f5d027e76b0f
Suqatri <paul@suqatri.net>
2023-06-06 18:04:35 +0200

    build: remove old logging framework

--- console/build.gradle.kts
+++ console/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     compileOnly(project(":utils"))
     compileOnly(project(":events"))
+    compileOnly(project(":logging"))
     compileOnly(project(":commands:command-api"))
 
     testImplementation(project(":commands:command-api"))
@@ -16,8 +17,4 @@ dependencies {
 
     dependency("org.jline:jline-console:3.23.0")
     dependency("org.jline:jline-terminal-jansi:3.23.0")
-
-    dependency("org.slf4j:slf4j-api:2.0.7")
-    dependency("ch.qos.logback:logback-core:1.3.7")
-    dependency("ch.qos.logback:logback-classic:1.3.7")
 }
