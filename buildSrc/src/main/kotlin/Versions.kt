675ab9d77addf10e5b119c11801e15affa6fec1a
Suqatri <paul@suqatri.net>
2023-06-06 13:21:41 +0200

    build: use libloader 1.6.7

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,8 +2,8 @@ object Versions {
     const val cloud = "1.0.0-SNAPSHOT"
     const val kotlin = "1.8.21"
     const val gson = "2.10.1"
-    const val libloader = "1.6.6"
-    const val libloaderBootstrap = "1.6.5"
+    const val libloader = "1.6.7"
+    const val libloaderBootstrap = "1.6.7"
     const val kotlinxCoroutines= "1.7.1"
     const val redisson = "3.21.3"
 }
