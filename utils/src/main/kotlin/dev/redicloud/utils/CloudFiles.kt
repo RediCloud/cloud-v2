2ff076a414fc792484b05ad2b760f26e233ffe0f
Suqatri <paul@suqatri.net>
2023-06-06 23:58:44 +0200

    feat: create cloud files

---
+++ utils/src/main/kotlin/dev/redicloud/utils/CloudFiles.kt
@@ -0,0 +1,53 @@
+package dev.redicloud.utils
+
+import java.io.File
+import java.nio.file.Paths
+
+private val cloudPath: String = System.getProperty("redicloud.cloud.path") ?: Paths.get("").toAbsolutePath().toString()
+
+class CloudFile(val name: String, val parent: String = "", val folder: Boolean = false) {
+    fun getFile(): File {
+        val path = if (parent.isEmpty()) {
+            cloudPath + File.separator + name
+        }else {
+            cloudPath + File.separator + parent + File.separator + name
+        }
+        return File(path)
+    }
+
+    fun createIfNotExists(): File {
+        val file = getFile()
+        if (file.exists()) return file
+        if (!file.parentFile.exists()) file.parentFile.mkdirs()
+        if (folder) {
+            file.mkdir()
+        }else {
+            file.createNewFile()
+        }
+        return file
+    }
+
+    fun create(): File {
+        val file = getFile()
+        if (file.exists()) {
+            file.delete()
+        }
+        if (!file.parentFile.exists()) file.parentFile.mkdirs()
+        if (folder) {
+            file.mkdir()
+        }else {
+            file.createNewFile()
+        }
+        return file
+    }
+
+}
+
+val NODE_JSON = CloudFile("node.json")
+val TEMP_FOLDER = CloudFile("tmp", folder = true)
+val STATIC_FOLDER = CloudFile("static", folder = true)
+val STORAGE_FOLDER = CloudFile("storage", folder = true)
+val LOG_FOLDER = CloudFile("logs", "storage", folder = true)
+val MINECRAFT_VERSIONS_FOLDER = CloudFile("versions", "storage", folder = true)
+val CACHED_TEMPLATES = CloudFile("cachedTemplates", "storage", folder = true)
+val DATABASE_JSON = CloudFile("database.json", "storage")
