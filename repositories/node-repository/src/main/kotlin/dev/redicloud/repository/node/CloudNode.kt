a12810fa2742dabedd8604995fb2f47690efbc7e
Suqatri <paul@suqatri.net>
2023-06-07 19:38:52 +0200

    refactor: add master field to cloud node

--- repositories/node-repository/src/main/kotlin/dev/redicloud/repository/node/CloudNode.kt
+++ repositories/node-repository/src/main/kotlin/dev/redicloud/repository/node/CloudNode.kt
@@ -2,12 +2,13 @@ package dev.redicloud.repository.node
 
 import dev.redicloud.repository.service.CloudService
 import dev.redicloud.repository.service.ServiceSession
-import dev.redicloud.utils.ServiceId
+import dev.redicloud.utils.service.ServiceId
 
 class CloudNode(
     serviceId: ServiceId,
     sessions: MutableList<ServiceSession>,
-    private val hostedServers: MutableList<ServiceId>
+    private val hostedServers: MutableList<ServiceId>,
+    var master: Boolean
 ) : CloudService(serviceId, sessions) {
 
     fun getHostedServers(): List<ServiceId> = hostedServers.toList()
