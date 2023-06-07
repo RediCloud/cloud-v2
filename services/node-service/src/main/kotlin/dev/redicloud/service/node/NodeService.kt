560d52720ab7cf6cb8c9d7241298e691baec6b3e
Suqatri <paul@suqatri.net>
2023-06-07 19:43:25 +0200

    impl: node service class

--- services/node-service/src/main/kotlin/dev/redicloud/service/node/NodeService.kt
+++ services/node-service/src/main/kotlin/dev/redicloud/service/node/NodeService.kt
@@ -2,15 +2,53 @@ package dev.redicloud.service.node
 
 import dev.redicloud.database.config.DatabaseConfiguration
 import dev.redicloud.service.base.BaseService
+import dev.redicloud.service.node.console.NodeConsole
+import dev.redicloud.service.node.events.NodeDisconnectEvent
+import dev.redicloud.service.node.events.NodeSuspendEvent
 import dev.redicloud.service.node.repository.connect
-import dev.redicloud.utils.ServiceId
-import dev.redicloud.utils.ServiceType
+import dev.redicloud.service.node.repository.disconnect
+import dev.redicloud.service.node.tasks.NodeChooseMasterTask
+import dev.redicloud.service.node.tasks.NodePingTask
+import dev.redicloud.service.node.tasks.NodeSelfSuspendTask
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration.Companion.seconds
 
 class NodeService(databaseConfiguration: DatabaseConfiguration, val configuration: NodeConfiguration)
-    : BaseService(databaseConfiguration, ServiceId(configuration.uniqueId, ServiceType.NODE)) {
+    : BaseService(databaseConfiguration, configuration.toServiceId()) {
+
+    private val console: NodeConsole = NodeConsole(configuration, eventManager)
 
     init {
-        nodeRepository.connect()
+        runBlocking {
+            nodeRepository.connect(this@NodeService)
+            registerTasks()
+        }
+    }
+
+    override fun shutdown() {
+        LOGGER.info("Shutting down node service...")
+        runBlocking {
+            nodeRepository.disconnect(this@NodeService)
+            super.shutdown()
+        }
+    }
+
+    private fun registerTasks() {
+        taskManager.builder()
+            .task(NodeChooseMasterTask(nodeRepository))
+            .event(NodeDisconnectEvent::class)
+            .event(NodeSuspendEvent::class)
+            .register()
+        taskManager.builder()
+            .task(NodePingTask(this))
+            .event(NodeDisconnectEvent::class)
+            .period(10.seconds)
+            .register()
+        taskManager.builder()
+            .task(NodeSelfSuspendTask(this))
+            .event(NodeSuspendEvent::class)
+            .period(10.seconds)
+            .register()
     }
 
 }
