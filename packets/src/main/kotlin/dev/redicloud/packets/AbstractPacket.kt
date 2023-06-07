a33d472e2446bdff84b77d3f2ee2651f0858787f
Suqatri <paul@suqatri.net>
2023-06-07 19:38:36 +0200

    feat: packet responses with timeout

--- packets/src/main/kotlin/dev/redicloud/packets/AbstractPacket.kt
+++ packets/src/main/kotlin/dev/redicloud/packets/AbstractPacket.kt
@@ -1,6 +1,6 @@
 package dev.redicloud.packets
 
-import dev.redicloud.utils.ServiceId
+import dev.redicloud.utils.service.ServiceId
 import java.util.*
 
 abstract class AbstractPacket {
@@ -8,7 +8,20 @@ abstract class AbstractPacket {
     val packetId: UUID = UUID.randomUUID()
     var sender: ServiceId? = null
     var allowLocalReceiver: Boolean = false
+    internal var manager: PacketManager? = null
+    internal var referenceId: UUID? = null
 
-    fun received() {}
+    open fun received() {}
+
+    suspend fun respond(packet: AbstractPacket) {
+        if (sender == null) return
+        if (manager == null) return
+        packet.referenceId = packetId
+        manager!!.publish(packet, sender!!)
+    }
+
+    fun getReferenceId(): UUID? = referenceId
+
+    fun getManager(): PacketManager? = manager
 
 }
--- packets/src/main/kotlin/dev/redicloud/packets/PacketManager.kt
+++ packets/src/main/kotlin/dev/redicloud/packets/PacketManager.kt
@@ -2,12 +2,22 @@ package dev.redicloud.packets
 
 import com.google.gson.Gson
 import dev.redicloud.database.DatabaseConnection
-import dev.redicloud.utils.ServiceId
-import dev.redicloud.utils.ServiceType
+import dev.redicloud.logging.LogManager
+import dev.redicloud.utils.service.ServiceId
+import dev.redicloud.utils.service.ServiceType
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.redisson.api.RTopic
 import org.redisson.api.listener.MessageListener
+import kotlin.reflect.KClass
+import kotlin.time.Duration.Companion.seconds
 
-class PacketManager(private val databaseConnection: DatabaseConnection, private val serviceId: ServiceId) {
+class PacketManager(private val databaseConnection: DatabaseConnection, val serviceId: ServiceId) {
+
+    companion object {
+        private val LOGGER = LogManager.logger(PacketManager::class.java)
+    }
 
     private val registeredPackets = mutableListOf<AbstractPacket>()
     private val serviceTopic: RTopic
@@ -15,6 +25,8 @@ class PacketManager(private val databaseConnection: DatabaseConnection, private 
     private val typedTopics: MutableMap<ServiceType, RTopic> = mutableMapOf()
     val gson = Gson()
     val listeners = mutableListOf<PacketListener<out AbstractPacket>>()
+    internal val packetResponses = mutableListOf<PacketResponse>()
+    internal val packetsOfLast3Seconds = mutableListOf<AbstractPacket>()
 
     init {
         if (!databaseConnection.isConnected()) throw IllegalStateException("Database connection is not connected!")
@@ -30,11 +42,27 @@ class PacketManager(private val databaseConnection: DatabaseConnection, private 
             val p = registeredPackets.firstOrNull { it::class.java.name == messageData.clazz }
                 ?: return@MessageListener
             val packet = gson.fromJson(data, p::class.java)
+            packet.manager = this
             packet.received()
+            packetsOfLast3Seconds.add(packet)
+            GlobalScope.launch {
+                delay(3.seconds)
+                packetsOfLast3Seconds.remove(packet)
+            }
+            packetResponses.forEach {
+                try {
+                    it.handle(packet)
+                }catch (e: Exception) {
+                    LOGGER.severe("Error while handling packet response ${p::class.java.simpleName}!", e)
+                }
+            }
             listeners.forEach {
                 if (it.packetClazz == p::class) {
-                    (it as PacketListener<AbstractPacket>).listener(packet)
-                    //TODO: catch errors and log
+                    try {
+                        (it as PacketListener<AbstractPacket>).listener(packet)
+                    } catch (e: Exception) {
+                        LOGGER.severe("Error while handling packet ${p::class.java.simpleName}!", e)
+                    }
                 }
             }
         }
@@ -59,28 +87,37 @@ class PacketManager(private val databaseConnection: DatabaseConnection, private 
         return listener
     }
 
+    fun <T : AbstractPacket> listen(clazz: KClass<T>, handler: (T) -> Unit): PacketListener<T> {
+        val listener = PacketListener(clazz, handler)
+        listeners.add(listener)
+        return listener
+    }
+
     fun unregisterListener(listener: PacketListener<out AbstractPacket>) {
         listeners.remove(listener)
     }
 
-    suspend fun publishPacket(packet: AbstractPacket, vararg receivers: ServiceId) {
+    suspend fun publish(packet: AbstractPacket, vararg receivers: ServiceId): PacketResponse {
         packet.sender = serviceId
         val packedPacket = PackedPacket(gson.toJson(packet), packet::class.java.name)
         receivers.forEach {
             val targetTopic = databaseConnection.getClient().getTopic(it.toName())
             targetTopic.publish(packedPacket)
         }
+        return PacketResponse(this, packet)
     }
 
-    suspend fun publishAll(packet: AbstractPacket) {
+    suspend fun publishAll(packet: AbstractPacket): PacketResponse {
         packet.sender = serviceId
         val packedPacket = PackedPacket(gson.toJson(packet), packet::class.java.name)
+        return PacketResponse(this, packet)
     }
 
-    suspend fun publish(packet: AbstractPacket, vararg serviceTypes: ServiceType) {
+    suspend fun publish(packet: AbstractPacket, vararg serviceTypes: ServiceType): PacketResponse {
         packet.sender = serviceId
         val packedPacket = PackedPacket(gson.toJson(packet), packet::class.java.name)
         serviceTypes.forEach { typedTopics[it]?.publish(packedPacket) }
+        return PacketResponse(this, packet)
     }
 
 }
