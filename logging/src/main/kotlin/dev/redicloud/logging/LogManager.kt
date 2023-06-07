4426defb4ff53c58ff921a195e57c71aed3a6692
Suqatri <paul@suqatri.net>
2023-06-07 19:38:18 +0200

    feat: logger by KClass

--- logging/src/main/kotlin/dev/redicloud/logging/LogManager.kt
+++ logging/src/main/kotlin/dev/redicloud/logging/LogManager.kt
@@ -2,6 +2,7 @@ package dev.redicloud.logging
 
 import dev.redicloud.logging.fallback.FallbackLoggerFactory
 import java.util.ServiceLoader
+import kotlin.reflect.KClass
 
 private val LOGGER_FACTORY: LoggerFactory = ServiceLoader.load(LoggerFactory::class.java).firstOrNull() ?: FallbackLoggerFactory()
 
@@ -13,6 +14,8 @@ class LogManager private constructor() {
         fun logger(name: String): Logger = LOGGER_FACTORY.getLogger(name)
 
         fun logger(clazz: Class<*>): Logger = LOGGER_FACTORY.getLogger(clazz.name)
+
+        fun logger(clazz: KClass<*>): Logger = LOGGER_FACTORY.getLogger(clazz.qualifiedName!!)
 
         fun logger(o: Any): Logger = LOGGER_FACTORY.getLogger(o.javaClass.name)
     }
