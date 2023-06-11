32c3431b15ad053cea8c36ef3f2a030d17251a94
Suqatri <paul@suqatri.net>
2023-06-11 16:56:50 +0200

    imports...

--- services/node-service/src/main/kotlin/dev/redicloud/service/node/bootstrap/NodeBootstrap.kt
+++ services/node-service/src/main/kotlin/dev/redicloud/service/node/bootstrap/NodeBootstrap.kt
@@ -1,6 +1,6 @@
 package dev.redicloud.service.node.bootstrap
 
-import dev.redicloud.repository.server.version.MinecraftVersion
+import dev.redicloud.repository.server.version.utils.ServerVersion
 import dev.redicloud.service.node.NodeService
 import dev.redicloud.service.node.console.InitializeConsole
 import dev.redicloud.utils.versions.JavaVersion
@@ -9,7 +9,7 @@ import kotlinx.coroutines.runBlocking
 fun main(args: Array<String>) {
     runBlocking {
         println("Starting node service...")
-        MinecraftVersion.loadIfNotLoaded()
+        ServerVersion.loadIfNotLoaded()
         JavaVersion.loadIfNotLoaded()
         val preConsole = InitializeConsole()
         val databaseConnection = preConsole.databaseConnection!!
