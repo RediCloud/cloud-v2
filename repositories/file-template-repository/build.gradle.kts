5bd6683143b4a4539a81340e4a7f8210c4c23f91
Suqatri <paul@suqatri.net>
2023-06-12 01:34:23 +0200

    build: update dependencies

--- repositories/file-template-repository/build.gradle.kts
+++ repositories/file-template-repository/build.gradle.kts
@@ -4,4 +4,5 @@ dependencies {
     compileOnly(project(":utils"))
     compileOnly(project(":database"))
     compileOnly(project(":packets"))
+    compileOnly(project(":repositories:node-repository"))
 }
