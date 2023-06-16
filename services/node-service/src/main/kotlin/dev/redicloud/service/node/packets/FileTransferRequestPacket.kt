68634d116e45c5589f7087bb0bf02762a475c8e8
Suqatri <paul@suqatri.net>
2023-06-16 15:12:19 +0200

    delete old packets / tasks

--- services/node-service/src/main/kotlin/dev/redicloud/service/node/packets/FileTransferRequestPacket.kt
+++
@@ -1,5 +0,0 @@
-package dev.redicloud.service.node.packets
-
-import dev.redicloud.packets.AbstractPacket
-
-class FileTransferRequestPacket(val cloudPath: String, val priority: Int, val deleteTargetBeforeUnzip: Boolean = false) : AbstractPacket()
--- services/node-service/src/main/kotlin/dev/redicloud/service/node/packets/FileTransferRequestResponse.kt
+++
@@ -1,6 +0,0 @@
-package dev.redicloud.service.node.packets
-
-import dev.redicloud.packets.AbstractPacket
-import java.util.UUID
-
-class FileTransferRequestResponse(val transferId: UUID?) : AbstractPacket()
