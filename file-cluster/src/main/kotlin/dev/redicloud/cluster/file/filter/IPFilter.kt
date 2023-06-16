2b4194b00ccad6acde05a8eab6be5e5b134f2172
Suqatri <paul@suqatri.net>
2023-06-16 15:11:09 +0200

    fix: not allowed sftp connections

---
+++ file-cluster/src/main/kotlin/dev/redicloud/cluster/file/filter/IPFilter.kt
@@ -0,0 +1,48 @@
+package dev.redicloud.cluster.file.filter
+
+import dev.redicloud.cluster.file.FileNodeRepository
+import dev.redicloud.cluster.file.event.FileNodeConnectedEvent
+import dev.redicloud.cluster.file.event.FileNodeDisconnectedEvent
+import dev.redicloud.event.EventManager
+import dev.redicloud.repository.node.NodeRepository
+import dev.redicloud.service.base.events.NodeConnectEvent
+import dev.redicloud.service.base.events.NodeSuspendedEvent
+import kotlinx.coroutines.runBlocking
+
+class IPFilter(
+    val eventManager: EventManager,
+    val fileNodeRepository: FileNodeRepository
+) {
+
+    private val allowedIpCache = mutableListOf<String>()
+    private val onNodeConnect = eventManager.listen<FileNodeConnectedEvent> {
+        runBlocking {
+            val fileNode = fileNodeRepository.getFileNode(it.serviceId) ?: return@runBlocking
+            val ipAddress = fileNode.hostname
+            allowedIpCache.add(ipAddress)
+        }
+    }
+    private val onNodeDisconnect = eventManager.listen<FileNodeDisconnectedEvent> {
+        runBlocking {
+            val fileNode = fileNodeRepository.getFileNode(it.serviceId) ?: return@runBlocking
+            val ipAddress = fileNode.hostname
+            allowedIpCache.remove(ipAddress)
+        }
+    }
+    private val onNodeSuspend = eventManager.listen<NodeSuspendedEvent> {
+        val ipAddress = it.node.currentOrLastsession()?.ipAddress ?: return@listen
+        allowedIpCache.remove(ipAddress)
+    }
+
+    init {
+        runBlocking {
+            System.getProperty("redicloud.filter.ip.bypass", "127.0.0.1;0.0.0.0").split(";").forEach {
+                allowedIpCache.add(it)
+            }
+            fileNodeRepository.getAll().forEach { allowedIpCache.add(it.hostname) }
+        }
+    }
+
+    fun canConnect(remoteAddress: String): Boolean = allowedIpCache.contains(remoteAddress)
+
+}
