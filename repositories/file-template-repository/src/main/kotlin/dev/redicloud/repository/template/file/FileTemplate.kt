23cc0f8529075a359eb1991819f45b64bf220334
Suqatri <paul@suqatri.net>
2023-06-16 22:58:45 +0200

    add file template identifier instead of names

--- repositories/file-template-repository/src/main/kotlin/dev/redicloud/repository/template/file/FileTemplate.kt
+++ repositories/file-template-repository/src/main/kotlin/dev/redicloud/repository/template/file/FileTemplate.kt
@@ -2,8 +2,14 @@ package dev.redicloud.repository.template.file
 
 import dev.redicloud.utils.TEMPLATE_FOLDER
 import java.io.File
+import java.util.UUID
 
-data class FileTemplate(val name: String, val prefix: String, val inherited: MutableList<String>) {
+data class FileTemplate(
+    val uniqueId: UUID = UUID.randomUUID(),
+    var name: String,
+    var prefix: String,
+    val inherited: MutableList<String> = mutableListOf()
+) {
 
     fun getFolder(): File {
         val parent = File(TEMPLATE_FOLDER.getFile().absolutePath, prefix)
--- repositories/file-template-repository/src/main/kotlin/dev/redicloud/repository/template/file/FileTemplateRepository.kt
+++ repositories/file-template-repository/src/main/kotlin/dev/redicloud/repository/template/file/FileTemplateRepository.kt
@@ -4,28 +4,34 @@ import dev.redicloud.database.DatabaseConnection
 import dev.redicloud.database.repository.DatabaseBucketRepository
 import dev.redicloud.packets.PacketManager
 import dev.redicloud.repository.node.NodeRepository
+import java.util.UUID
 
 class FileTemplateRepository(databaseConnection: DatabaseConnection, val packetManager: PacketManager, val nodeRepository: NodeRepository) :
     DatabaseBucketRepository<FileTemplate>(databaseConnection, "file-template") {
 
+    suspend fun getTemplate(uniqueId: UUID): FileTemplate? {
+        return getHandle("$uniqueId").get()
+    }
+
     suspend fun getTemplate(name: String, prefix: String): FileTemplate? {
-        return getHandle("${prefix.lowercase()}:${name.lowercase()}").get()
+        return getTemplates().firstOrNull { it.name == name && it.prefix == prefix }
+    }
+
+    suspend fun existsTemplate(uniqueId: UUID, prefix: String): Boolean {
+        return getHandle("$uniqueId").isExists
     }
 
     suspend fun existsTemplate(name: String, prefix: String): Boolean {
-        return getHandle("${prefix.lowercase()}:${name.lowercase()}").isExists
+        return getTemplates().any { it.name == name && it.prefix == prefix }
     }
 
-    suspend fun updateTemplate(template: FileTemplate, oldPrefix: String? = null, oldName: String? = null): FileTemplate {
-        var identifier = oldPrefix ?: template.prefix
-        identifier += oldName ?: template.name
-        identifier = identifier.lowercase()
-        getHandle(identifier).set(template)
+    suspend fun updateTemplate(template: FileTemplate): FileTemplate {
+        getHandle("${template.uniqueId}").set(template)
         return template
     }
 
     suspend fun createTemplate(template: FileTemplate): FileTemplate {
-        getHandle("${template.prefix.lowercase()}:${template.name.lowercase()}").set(template)
+        getHandle("${template.uniqueId}").set(template)
         return template
     }
 
