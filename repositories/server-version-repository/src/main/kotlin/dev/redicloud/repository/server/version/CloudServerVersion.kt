b656a6fcbc87a2455f44c532d1afd79d3c5269ec
Suqatri <paul@suqatri.net>
2023-06-19 19:28:33 +0200

    refactor: server version now identified by uuid

--- repositories/server-version-repository/src/main/kotlin/dev/redicloud/repository/server/version/CloudServerVersion.kt
+++ repositories/server-version-repository/src/main/kotlin/dev/redicloud/repository/server/version/CloudServerVersion.kt
@@ -2,8 +2,10 @@ package dev.redicloud.repository.server.version
 
 import dev.redicloud.repository.server.version.utils.CloudServerVersionType
 import dev.redicloud.repository.server.version.utils.ServerVersion
+import java.util.UUID
 
 class CloudServerVersion(
+    val uniqueId: UUID,
     val type: CloudServerVersionType,
     val name: String,
     val customDownloadUrl: String?,
