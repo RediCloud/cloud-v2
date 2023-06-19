d5d9bad852b0b0c5232fb7929f00226b2ec9fb45
Suqatri <paul@suqatri.net>
2023-06-19 19:27:12 +0200

    fix: db object by name

--- repositories/server-version-repository/src/main/kotlin/dev/redicloud/repository/server/version/ServerVersionRepository.kt
+++ repositories/server-version-repository/src/main/kotlin/dev/redicloud/repository/server/version/ServerVersionRepository.kt
@@ -2,29 +2,33 @@ package dev.redicloud.repository.server.version
 
 import dev.redicloud.database.DatabaseConnection
 import dev.redicloud.database.repository.DatabaseBucketRepository
+import java.util.*
 
 class ServerVersionRepository(databaseConnection: DatabaseConnection) : DatabaseBucketRepository<CloudServerVersion>(databaseConnection, "server-version:"){
 
+    suspend fun getVersion(uniqueId: UUID): CloudServerVersion? {
+        return getHandle(uniqueId.toString()).get()
+    }
+
     suspend fun getVersion(name: String): CloudServerVersion? {
-        return getHandle(name.lowercase()).get()
+        return getVersions().firstOrNull { it.name.lowercase() == name.lowercase() }
     }
 
-    suspend fun existsVersion(name: String): Boolean {
-        return getHandle(name.lowercase()).isExists
+    suspend fun existsVersion(uniqueId: UUID): Boolean {
+        return getHandle(uniqueId.toString()).isExists
     }
 
-    suspend fun updateVersion(serverVersion: CloudServerVersion, oldName: String? = null): CloudServerVersion {
-        return if (oldName != null) {
-            getHandle(oldName.lowercase()).set(serverVersion)
-            serverVersion
-        }else {
-            getHandle(serverVersion.name.lowercase())
-            serverVersion
-        }
+    suspend fun existsVersion(name: String): CloudServerVersion? {
+        return getVersions().firstOrNull { it.name.lowercase() == name.lowercase() }
+    }
+
+    suspend fun updateVersion(serverVersion: CloudServerVersion): CloudServerVersion {
+        getHandle(serverVersion.uniqueId.toString()).set(serverVersion)
+        return serverVersion
     }
 
     suspend fun createVersion(serverVersion: CloudServerVersion): CloudServerVersion {
-        getHandle(serverVersion.name.lowercase()).set(serverVersion)
+        getHandle(serverVersion.uniqueId.toString()).set(serverVersion)
         return serverVersion
     }
 
