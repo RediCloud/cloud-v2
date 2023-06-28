dc4dfe5d946d427cca2ca9c9175fc852e3060430
Suqatri <paul@suqatri.net>
2023-06-29 01:33:29 +0200

    perf: improve command argument suggester

--- commands/command-api/src/main/kotlin/dev/redicloud/commands/api/ICommandSuggester.kt
+++ commands/command-api/src/main/kotlin/dev/redicloud/commands/api/ICommandSuggester.kt
@@ -1,5 +1,8 @@
 package dev.redicloud.commands.api
 
+import dev.redicloud.utils.EasyCache
+import kotlin.time.Duration.Companion.seconds
+
 interface ICommandSuggester {
     companion object {
         val SUGGESTERS = mutableListOf(
@@ -63,20 +66,25 @@ class CommandSuggester(val command: CommandBase) : ICommandSuggester {
 }
 
 class CommandArgumentSuggester(val commandArgument: CommandArgument) : ICommandSuggester {
-    override fun suggest(context: CommandSuggesterContext): Array<String> {
-        if (!commandArgument.isThis(context.input, true)) return arrayOf()
+
+    private val easyCache = EasyCache<Array<String>, CommandSuggesterContext>(5.seconds) { context ->
+        if (context == null) return@EasyCache arrayOf()
+        if (!commandArgument.isThis(context.input, true)) return@EasyCache arrayOf()
         val nextArgument = !commandArgument.isThis(context.input, false)
         if (nextArgument) {
             val input = context.input.removeLastSpaces().removeFirstSpaces()
-            return commandArgument.annotatedSuggester.suggest(context).map { "$input $it" }.toTypedArray()
+            return@EasyCache commandArgument.annotatedSuggester.suggest(context).map { "$input $it" }.toTypedArray()
         }
         val argumentOptions = commandArgument.annotatedSuggester.suggest(context)
         val input = context.input.removeLastSpaces().removeFirstSpaces()
         val lastArgument = input.split(" ").last()
         val currentPathWithoutArgument = input.split(" ").dropLast(1).joinToString(" ")
-        return commandArgument.annotatedSuggester.suggest(context)
+        return@EasyCache commandArgument.annotatedSuggester.suggest(context)
             .filter { lastArgument.lowercase().startsWith(it.lowercase()) }
             .map { "$currentPathWithoutArgument $it" }
             .toTypedArray()
     }
+
+    override fun suggest(context: CommandSuggesterContext): Array<String> = easyCache.get(context)!!
+
 }
