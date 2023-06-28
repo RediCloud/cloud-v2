8094533c0e10810fda1ca0f7fa7c5b9628069c7a
Suqatri <paul@suqatri.net>
2023-06-29 01:40:10 +0200

    Create CloudServerVersionSuggester.kt

---
+++ services/base-service/src/main/kotlin/dev/redicloud/service/base/suggester/CloudServerVersionSuggester.kt
@@ -0,0 +1,18 @@
+package dev.redicloud.service.base.suggester
+
+import dev.redicloud.commands.api.CommandSuggesterContext
+import dev.redicloud.commands.api.ICommandSuggester
+import dev.redicloud.repository.server.version.CloudServerVersion
+import dev.redicloud.repository.server.version.ServerVersionRepository
+import dev.redicloud.utils.EasyCache
+import kotlin.time.Duration.Companion.seconds
+
+class CloudServerVersionSuggester(
+    private val serverVersionRepository: ServerVersionRepository
+) : ICommandSuggester {
+
+    private val easyCache = EasyCache<List<CloudServerVersion>, Unit>(5.seconds) { serverVersionRepository.getAll() }
+
+    override fun suggest(context: CommandSuggesterContext): Array<String> =
+        easyCache.get()?.map { it.name }?.toTypedArray() ?: emptyArray()
+}
