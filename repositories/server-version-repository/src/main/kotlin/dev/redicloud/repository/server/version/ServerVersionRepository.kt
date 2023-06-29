d836ffce244e4f6b354dbadfe833cc29f27e7969
Suqatri <paul@suqatri.net>
2023-06-29 23:42:27 +0200

    fix: get all bucket objects

--- repositories/server-version-repository/src/main/kotlin/dev/redicloud/repository/server/version/ServerVersionRepository.kt
+++ repositories/server-version-repository/src/main/kotlin/dev/redicloud/repository/server/version/ServerVersionRepository.kt
@@ -4,7 +4,7 @@ import dev.redicloud.database.DatabaseConnection
 import dev.redicloud.database.repository.DatabaseBucketRepository
 import java.util.*
 
-class ServerVersionRepository(databaseConnection: DatabaseConnection) : DatabaseBucketRepository<CloudServerVersion>(databaseConnection, "server-version:"){
+class ServerVersionRepository(databaseConnection: DatabaseConnection) : DatabaseBucketRepository<CloudServerVersion>(databaseConnection, "server-version"){
 
     suspend fun getVersion(uniqueId: UUID): CloudServerVersion? {
         return getHandle(uniqueId.toString()).get()
@@ -18,8 +18,8 @@ class ServerVersionRepository(databaseConnection: DatabaseConnection) : Database
         return getHandle(uniqueId.toString()).isExists
     }
 
-    suspend fun existsVersion(name: String): CloudServerVersion? {
-        return getVersions().firstOrNull { it.name.lowercase() == name.lowercase() }
+    suspend fun existsVersion(name: String): Boolean {
+        return getVersions().any { it.name.lowercase() == name.lowercase() }
     }
 
     suspend fun updateVersion(serverVersion: CloudServerVersion): CloudServerVersion {
@@ -30,6 +30,10 @@ class ServerVersionRepository(databaseConnection: DatabaseConnection) : Database
     suspend fun createVersion(serverVersion: CloudServerVersion): CloudServerVersion {
         getHandle(serverVersion.uniqueId.toString()).set(serverVersion)
         return serverVersion
+    }
+
+    suspend fun deleteVersion(uniqueId: UUID): Boolean {
+        return getHandle(uniqueId.toString()).delete()
     }
 
     suspend fun getVersions(): List<CloudServerVersion> = getAll()
