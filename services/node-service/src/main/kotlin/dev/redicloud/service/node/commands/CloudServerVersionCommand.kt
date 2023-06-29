b35174fe80c6c6b57e706c9308a46a837495050c
Suqatri <paul@suqatri.net>
2023-06-29 23:43:21 +0200

    Update CloudServerVersionCommand.kt

--- services/node-service/src/main/kotlin/dev/redicloud/service/node/commands/CloudServerVersionCommand.kt
+++ services/node-service/src/main/kotlin/dev/redicloud/service/node/commands/CloudServerVersionCommand.kt
@@ -2,10 +2,12 @@ package dev.redicloud.service.node.commands
 
 import dev.redicloud.commands.api.*
 import dev.redicloud.console.commands.ConsoleActor
+import dev.redicloud.repository.server.ServerRepository
 import dev.redicloud.repository.server.version.CloudServerVersion
 import dev.redicloud.repository.server.version.ServerVersionRepository
 import dev.redicloud.repository.server.version.utils.CloudServerVersionType
 import dev.redicloud.repository.server.version.utils.ServerVersion
+import dev.redicloud.repository.template.configuration.ConfigurationTemplateRepository
 import dev.redicloud.service.base.suggester.CloudServerVersionSuggester
 import dev.redicloud.service.base.suggester.ServerVersionSuggester
 import kotlinx.coroutines.runBlocking
@@ -15,16 +17,22 @@ import java.util.*
 @CommandAlias(["serverversion", "serverversion"])
 @CommandDescription("Configures the server version")
 class CloudServerVersionCommand(
-    private val serverVersionRepository: ServerVersionRepository
+    private val serverVersionRepository: ServerVersionRepository,
+    private val configurationTemplateRepository: ConfigurationTemplateRepository,
+    private val serverRepository: ServerRepository
 ) : CommandBase() {
 
-    @CommandSubPath("edit <version> name <Name>")
+    @CommandSubPath("edit <version> name <name>")
     @CommandDescription("Set the name of the server version")
     fun onEditName(
         actor: ConsoleActor,
         @CommandParameter("version", true, CloudServerVersionSuggester::class) version: CloudServerVersion,
         @CommandParameter("name", true) name: String) {
         runBlocking {
+            if (serverVersionRepository.existsVersion(name)) {
+                actor.sendMessage("§cA version with the name '$name' already exists!")
+                return@runBlocking
+            }
             val oldName = version.name
             version.name = name
             serverVersionRepository.updateVersion(version)
@@ -32,20 +40,20 @@ class CloudServerVersionCommand(
         }
     }
 
-    @CommandSubPath("edit <version> url <URL>")
+    @CommandSubPath("edit <version> url <url>")
     @CommandDescription("Set the download url of the server version")
     fun onEditUrl(
         actor: ConsoleActor,
         @CommandParameter("version", true, CloudServerVersionSuggester::class) version: CloudServerVersion,
         @CommandParameter("url", true) url: String) {
         runBlocking {
-            version.customDownloadUrl = url
+            version.customDownloadUrl = if (url != "null") url else null
             serverVersionRepository.updateVersion(version)
             actor.sendMessage("Updated download url of ${version.name} to ${version.customDownloadUrl}")
         }
     }
 
-    @CommandSubPath("edit <version> type <Type>")
+    @CommandSubPath("edit <version> type <type>")
     @CommandDescription("Set the type of the server version")
     fun onEditType(
         actor: ConsoleActor,
@@ -64,7 +72,7 @@ class CloudServerVersionCommand(
         }
     }
 
-    @CommandSubPath("edit <version> libPattern <Patter>")
+    @CommandSubPath("edit <version> libPattern <pattern>")
     @CommandDescription("Set the lib pattern for the files that should be stored after the patch. Set to 'null' to disable the patching")
     fun onEditLibPattern(
         actor: ConsoleActor,
@@ -77,7 +85,7 @@ class CloudServerVersionCommand(
         }
     }
 
-    @CommandSubPath("edit <version> version <Version>")
+    @CommandSubPath("edit <version> version <mcversion>")
     @CommandDescription("Set the minecraft version of the server version")
     fun onEditMinecraftVersion(
         actor: ConsoleActor,
@@ -96,12 +104,16 @@ class CloudServerVersionCommand(
         }
     }
 
-    @CommandSubPath("create <version>")
+    @CommandSubPath("create <name>")
     @CommandDescription("Create a new server version")
     fun onCreate(
         actor: ConsoleActor,
         @CommandParameter("name", true) versionName: String) {
         runBlocking {
+            if (serverVersionRepository.existsVersion(versionName)) {
+                actor.sendMessage("§cA server version with the name $versionName already exists!")
+                return@runBlocking
+            }
             val version = CloudServerVersion(
                 UUID.randomUUID(),
                 CloudServerVersionType.UNKNOWN,
@@ -115,6 +127,66 @@ class CloudServerVersionCommand(
             actor.sendMessage("§aCreated server version with name ${version.name}")
             actor.sendMessage("Use '/sv edit ${version.name} <key> <value>' to edit the server version")
         }
+    }
+
+    @CommandSubPath("delete <version>")
+    @CommandDescription("Delete a server version")
+    fun onDelete(
+        actor: ConsoleActor,
+        @CommandParameter("version", true, CloudServerVersionSuggester::class) version: CloudServerVersion
+    ) {
+        runBlocking {
+            val servers = serverRepository.getConnectedServers()
+                .filter { it.configurationTemplate.serverVersionId == version.uniqueId }
+            if (servers.isNotEmpty() ) {
+                actor.sendMessage("§cThere are still servers connected to this version:")
+                servers.forEach {
+                    actor.sendMessage("§8- %hc%${it.serviceId.toName()}")
+                }
+                return@runBlocking
+            }
+            val templates = configurationTemplateRepository.getTemplates()
+                .filter { it.serverVersionId == version.uniqueId }
+            if (templates.isNotEmpty()) {
+                actor.sendMessage("§cThere are still configuration templates using this version:")
+                templates.forEach {
+                    actor.sendMessage("§8- %hc%${it.name}")
+                }
+                return@runBlocking
+            }
+            serverVersionRepository.deleteVersion(version.uniqueId)
+            actor.sendMessage("Deleted server version with name ${version.name}")
+        }
+    }
+
+    @CommandSubPath("list")
+    @CommandDescription("List all versions")
+    fun onCreate(
+        actor: ConsoleActor
+    ) {
+        runBlocking {
+            actor.sendMessage("Server versions:")
+            serverVersionRepository.getVersions().forEach {
+                actor.sendMessage("§8- %hc%${it.name} §8(%tc%${it.type.name}§8)")
+            }
+        }
+    }
+
+    @CommandSubPath("info <version>")
+    @CommandDescription("Get info about a version")
+    fun onInfo(
+        actor: ConsoleActor,
+        @CommandParameter("version", true, CloudServerVersionSuggester::class) version: CloudServerVersion
+    ) {
+        actor.sendMessage("§8<====== %hc%§nServer-Version info §8 ======§8>")
+        actor.sendMessage("")
+        actor.sendMessage("§8- %hc%Name§8: %tc%${version.name}")
+        actor.sendMessage("§8- %hc%Type§8: %tc%${version.type.name}")
+        actor.sendMessage("§8- %hc%Lib-Pattern§8: %tc%${version.libPattern}")
+        actor.sendMessage("§8- %hc%Version§8: %tc%${version.version.name}")
+        actor.sendMessage("§8- %hc%Download-Url§8: %tc%${version.customDownloadUrl}")
+        actor.sendMessage("")
+        actor.sendMessage("§8<====== %hc%§nServer-Version info §8 ======§8>")
     }
 
 }
