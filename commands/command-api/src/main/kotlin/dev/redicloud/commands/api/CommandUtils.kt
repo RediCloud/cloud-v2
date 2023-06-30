9325dd25aaf48ed4bcd8a51bec1a9af06a598172
Suqatri <paul@suqatri.net>
2023-06-30 02:44:06 +0200

    Update CommandUtils.kt

--- commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandUtils.kt
+++ commands/command-api/src/main/kotlin/dev/redicloud/commands/api/CommandUtils.kt
@@ -3,3 +3,5 @@ package dev.redicloud.commands.api
 fun String.isOptionalArgument(): Boolean = this.startsWith("[") && this.endsWith("]")
 
 fun String.isRequiredArgument(): Boolean = this.startsWith("<") && this.endsWith(">")
+
+fun String.isArgument(): Boolean = isOptionalArgument() || isRequiredArgument()
