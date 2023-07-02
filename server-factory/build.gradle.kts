0eb6c69861a9ff17de846cee24a3091f9883d03e
Suqatri <paul@suqatri.net>
2023-07-02 20:00:13 +0200

    Update build.gradle.kts

--- server-factory/build.gradle.kts
+++ server-factory/build.gradle.kts
@@ -2,8 +2,10 @@ group = "dev.redicloud.server.factory"
 
 dependencies {
     compileOnly(project(":utils"))
+    compileOnly(project(":tasks"))
     compileOnly(project(":database"))
     compileOnly(project(":logging"))
+    compileOnly(project(":services:base-service"))
     compileOnly(project(":events"))
     compileOnly(project(":repositories:server-repository"))
     compileOnly(project(":repositories:node-repository"))
