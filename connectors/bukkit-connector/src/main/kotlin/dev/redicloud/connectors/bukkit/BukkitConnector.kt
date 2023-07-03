cc3e5e70d63fcccd23a011b5006ebe96272a1e08
Suqatri <paul@suqatri.net>
2023-07-03 16:25:36 +0200

    rewrite bukkit-connector

--- connectors/bukkit-connector/src/main/kotlin/dev/redicloud/connectors/bukkit/BukkitConnector.kt
+++ connectors/bukkit-connector/src/main/kotlin/dev/redicloud/connectors/bukkit/BukkitConnector.kt
@@ -1,58 +1,11 @@
 package dev.redicloud.connectors.bukkit
 
-import dev.redicloud.api.server.CloudServerState
-import dev.redicloud.connectors.bukkit.repositories.connect
-import dev.redicloud.connectors.bukkit.repositories.disconnect
-import dev.redicloud.database.config.DatabaseConfiguration
-import dev.redicloud.logging.LogManager
-import dev.redicloud.repository.server.version.CloudServerVersionTypeRepository
-import dev.redicloud.repository.template.file.AbstractFileTemplateRepository
-import dev.redicloud.service.base.BaseService
-import dev.redicloud.service.base.repository.BaseFileTemplateRepository
-import dev.redicloud.utils.DATABASE_JSON
-import dev.redicloud.utils.service.ServiceId
-import kotlinx.coroutines.runBlocking
+import dev.redicloud.service.minecraft.MinecraftServerService
 import org.bukkit.plugin.java.JavaPlugin
 
-class BukkitConnector(val plugin: JavaPlugin) : BaseService(
-    DatabaseConfiguration.fromFile(DATABASE_JSON.getFile()),
-    null,
-    ServiceId.fromString(System.getenv("RC_SERVICE_ID"))
-) {
-
-    override val fileTemplateRepository: AbstractFileTemplateRepository
-    override val serverVersionTypeRepository: CloudServerVersionTypeRepository
-    val logger = LogManager.Companion.logger("BukkitConnector")
-
-    init {
-        fileTemplateRepository = BaseFileTemplateRepository(this.databaseConnection, this.nodeRepository)
-        serverVersionTypeRepository = CloudServerVersionTypeRepository(this.databaseConnection, null)
-
-        runBlocking {
-            registerDefaults()
-            serverRepository.connect(serviceId)
-        }
-    }
-
-    fun onEnable() = runBlocking {
-        try {
-            val server = serverRepository.getServer(serviceId) ?: throw IllegalStateException("Server not found!")
-            server.state = CloudServerState.RUNNING
-            serverRepository.updateServer(server)
-            logger.info("Enabled cloud connector for server ${server.getIdentifyingName(false)}!")
-        } catch (e: Exception) {
-            logger.severe("Failed to enable cloud connector! Stopping server...", e)
-            shutdown()
-        }
-    }
-
-    override fun shutdown() {
-        if (SHUTTINGDOWN) return
-        SHUTTINGDOWN = true
-        runBlocking {
-            serverRepository.disconnect(serviceId)
-        }
-        super.shutdown()
+class BukkitConnector(val plugin: JavaPlugin) : MinecraftServerService<JavaPlugin>() {
+    override fun getConnectorPlugin(): JavaPlugin {
+        return plugin
     }
 
 }
--- connectors/bukkit-connector/src/main/kotlin/dev/redicloud/connectors/bukkit/bootstrap/BukkitConnectorBootstrap.kt
+++ connectors/bukkit-connector/src/main/kotlin/dev/redicloud/connectors/bukkit/bootstrap/BukkitConnectorBootstrap.kt
@@ -5,14 +5,20 @@ import dev.redicloud.libloader.boot.Bootstrap
 import dev.redicloud.libloader.boot.loaders.URLClassLoaderJarLoader
 import org.bukkit.plugin.java.JavaPlugin
 import java.net.URLClassLoader
+import kotlin.system.exitProcess
 
 class BukkitConnectorBootstrap : JavaPlugin() {
 
     private var connector: BukkitConnector? = null
 
     override fun onLoad() {
-        Bootstrap().apply(URLClassLoaderJarLoader(this::class.java.classLoader as URLClassLoader))
-        connector = BukkitConnector(this)
+        try {
+            Bootstrap().apply(URLClassLoaderJarLoader(this::class.java.classLoader as URLClassLoader))
+            connector = BukkitConnector(this)
+        }catch (e: Exception) {
+            e.printStackTrace()
+            onDisable()
+        }
     }
 
     override fun onEnable() {
@@ -20,6 +26,9 @@ class BukkitConnectorBootstrap : JavaPlugin() {
     }
 
     override fun onDisable() {
+        if (connector == null) {
+            exitProcess(0)
+        }
         connector?.logger?.info("Disabling cloud connector...")
         connector?.shutdown()
     }
