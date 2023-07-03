e4ebaeb785aacf3ef43578044c4ccf9b95240880
Suqatri <paul@suqatri.net>
2023-07-03 16:21:59 +0200

    move server repo connect/disconnect extensions to minecraft service project

--- services/minecraft-server-service/src/main/kotlin/dev/redicloud/service/minecraft/repositories/CloudServerRepositoryExtensions.kt
+++ services/minecraft-server-service/src/main/kotlin/dev/redicloud/service/minecraft/repositories/CloudServerRepositoryExtensions.kt
@@ -1,16 +1,17 @@
-package dev.redicloud.connectors.bukkit.repositories
+package dev.redicloud.service.minecraft.repositories
 
+import dev.redicloud.repository.server.CloudServer
 import dev.redicloud.repository.server.ServerRepository
 import dev.redicloud.utils.service.ServiceId
 
 suspend fun ServerRepository.connect(serviceId: ServiceId) {
-    val thisServer = this.getServer(serviceId) ?: throw IllegalStateException("Server not found")
+    val thisServer = this.getServer<CloudServer>(serviceId) ?: throw IllegalStateException("Server not found")
     thisServer.connected = true
     updateServer(thisServer)
 }
 
 suspend fun ServerRepository.disconnect(serviceId: ServiceId) {
-    val thisServer = this.getServer(serviceId) ?: throw IllegalStateException("Server not found")
+    val thisServer = this.getServer<CloudServer>(serviceId) ?: throw IllegalStateException("Server not found")
     thisServer.connected = false
     updateServer(thisServer)
 }
