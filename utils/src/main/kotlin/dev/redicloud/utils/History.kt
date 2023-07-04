e4dc1acb7d25bf3b1e322ac2e13ce7e4339d7b59
Suqatri <paul@suqatri.net>
2023-07-04 19:08:03 +0200

    fix: history order

--- utils/src/main/kotlin/dev/redicloud/utils/History.kt
+++ utils/src/main/kotlin/dev/redicloud/utils/History.kt
@@ -1,23 +1,40 @@
 package dev.redicloud.utils
 
-import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.ConcurrentHashMap
 
-class History<T>(val historySize: Int) : ConcurrentLinkedQueue<T>() {
+class History<T : Any>(val historySize: Int) {
 
-    override fun offer(e: T): Boolean {
-        this.checkSize()
-        return super.offer(e)
+    private val map = ConcurrentHashMap<Long, T>()
+
+    val size: Int
+        get() = map.size
+
+    fun add(value: T) {
+        map[System.nanoTime()] = value
+        checkSize()
+    }
+
+    fun remove(key: Long, value: T) {
+        map.remove(key, value)
     }
 
     fun subList(fromIndex: Int, toIndex: Int): List<T> {
-        return this.toList().subList(fromIndex, toIndex)
+        val f = if (fromIndex < 0) 0 else fromIndex
+        val t = if (toIndex > size) size else toIndex
+        return map.values.toList().subList(f, t)
     }
 
     private fun checkSize() {
-        if (size > historySize && isNotEmpty()) return
+        if (size > historySize && map.isNotEmpty()) return
         while (size > historySize) {
-            poll()
+            map.minBy { it.key }.let {
+                map.remove(it.key)
+            }
         }
+    }
+
+    fun forEach(action: (T) -> Unit) {
+        map.toSortedMap { o1, o2 -> o1.compareTo(o2) }.values.toList().forEach(action)
     }
 
 }
