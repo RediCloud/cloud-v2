a769063031347504d06319cfdc875badffa127d5
Suqatri <paul@suqatri.net>
2023-07-13 19:54:37 +0200

    fix: property init

--- services/node-service/src/main/kotlin/dev/redicloud/service/node/bootstrap/NodeBootstrap.kt
+++ services/node-service/src/main/kotlin/dev/redicloud/service/node/bootstrap/NodeBootstrap.kt
@@ -2,6 +2,7 @@ package dev.redicloud.service.node.bootstrap
 
 import dev.redicloud.service.node.NodeService
 import dev.redicloud.service.node.console.InitializeConsole
+import dev.redicloud.utils.loadProperties
 import kotlinx.coroutines.runBlocking
 import kotlin.system.exitProcess
 
@@ -9,6 +10,7 @@ fun main(args: Array<String>) {
     runBlocking {
         println("Starting node service...")
         try {
+            loadProperties(Thread.currentThread().contextClassLoader)
             val preConsole = InitializeConsole()
             val databaseConnection = preConsole.databaseConnection!!
             val databaseConfiguration = preConsole.databaseConfiguration!!
