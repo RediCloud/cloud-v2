4cd4708367302909f19ef07c62a4d37ac7e634e1
Suqatri <paul@suqatri.net>
2023-07-14 17:39:07 +0200

    update redisson to 3.23.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val libloader = "1.6.7"
     const val libloaderBootstrap = "1.6.7"
     const val kotlinxCoroutines= "1.7.1"
-    const val redisson = "3.21.3"
+    const val redisson = "3.23.0"
     const val slf4j = "2.0.7"
     const val khttp = "0.1.0"
     const val minecraftVersion = "1.20.1-R0.1-SNAPSHOT"
