72fca5144eaafd659ff668cdc2d622603bb7a31d
Suqatri <paul@suqatri.net>
2023-07-19 00:58:47 +0200

    data classes to store module informations

---
+++ modules/module-handler/src/main/kotlin/dev/redicloud/modules/ModuleData.kt
@@ -0,0 +1,14 @@
+package dev.redicloud.modules
+
+import dev.redicloud.api.modules.ICloudModule
+import dev.redicloud.api.modules.ModuleLifeCycle
+import java.io.File
+
+data class ModuleData<T : ICloudModule>(
+    val id: String,
+    val module: T,
+    val file: File,
+    val description: ModuleDescription,
+    var lifeCycle: ModuleLifeCycle,
+    var loader: ModuleClassLoader
+)
