4a95586b2007d5d74f88d3a3c683cf77b5c0cc12
Suqatri <paul@suqatri.net>
2023-07-20 01:11:14 +0200

    update example module methods

--- examples/example-module/src/main/kotlin/dev/redicloud/example/ExampleModule.kt
+++ examples/example-module/src/main/kotlin/dev/redicloud/example/ExampleModule.kt
@@ -1,5 +1,6 @@
 package dev.redicloud.example
 
+import com.google.inject.Inject
 import com.google.inject.name.Named
 import dev.redicloud.api.modules.ICloudModule
 import dev.redicloud.api.modules.ModuleLifeCycle
@@ -7,37 +8,16 @@ import dev.redicloud.api.modules.ModuleTask
 import dev.redicloud.api.modules.ModuleTaskOrder
 import dev.redicloud.api.service.ServiceId
 import dev.redicloud.api.utils.CloudInjectable
-import java.util.logging.LogManager
+import java.util.logging.Logger
 
-class ExampleModule : ICloudModule, CloudInjectable {
 
-    companion object {
-        private val logger = LogManager.getLogManager().getLogger(ExampleModule::class.java.name)
-    }
-
-    @ModuleTask(ModuleLifeCycle.LOAD, ModuleTaskOrder.FIRST)
-    fun first(@Named("this") serviceId: ServiceId) {
-        logger.info("First task: ${serviceId.toName()}")
-    }
+class ExampleModule @Inject constructor(
+    @Named("service") val logger: Logger
+) : ICloudModule, CloudInjectable {
 
     @ModuleTask(ModuleLifeCycle.LOAD, ModuleTaskOrder.EARLY)
     fun early(@Named("this") serviceId: ServiceId) {
         logger.info("Early task: ${serviceId.toName()}")
-    }
-
-    @ModuleTask(ModuleLifeCycle.LOAD, ModuleTaskOrder.DEFAULT)
-    fun default(@Named("this") serviceId: ServiceId) {
-        logger.info("Default task: ${serviceId.toName()}")
-    }
-
-    @ModuleTask(ModuleLifeCycle.LOAD, ModuleTaskOrder.LATE)
-    fun late(@Named("this") serviceId: ServiceId) {
-        logger.info("Late task: ${serviceId.toName()}")
-    }
-
-    @ModuleTask(ModuleLifeCycle.LOAD, ModuleTaskOrder.LAST)
-    fun last(@Named("this") serviceId: ServiceId) {
-        logger.info("Last task: ${serviceId.toName()}")
     }
 
     @ModuleTask(ModuleLifeCycle.UNLOAD, ModuleTaskOrder.FIRST)
