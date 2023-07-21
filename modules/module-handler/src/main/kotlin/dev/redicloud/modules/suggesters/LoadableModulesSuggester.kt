86531fca51187921dad97630359f8aa3de499610
Suqatri <paul@suqatri.net>
2023-07-21 23:45:45 +0200

    fix: name of loadable module suggester

--- modules/module-handler/src/main/kotlin/dev/redicloud/modules/suggesters/LoadableModulesSuggester.kt
+++ modules/module-handler/src/main/kotlin/dev/redicloud/modules/suggesters/LoadableModulesSuggester.kt
@@ -5,18 +5,28 @@ import dev.redicloud.api.commands.CommandContext
 import dev.redicloud.api.utils.MODULE_FOLDER
 import dev.redicloud.modules.ModuleHandler
 
-class LoadablesModulesSuggester(
+class LoadableModulesSuggester(
     private val moduleHandler: ModuleHandler
 ) : AbstractCommandSuggester() {
 
     override fun suggest(context: CommandContext): Array<String> {
         val descriptions = moduleHandler.getCachedDescriptions()
-            .filter { moduleHandler.getModuleData(it.id)?.loaded == true }
+            .filter {
+                val data = moduleHandler.getModuleData(it.id)
+                data == null || data != null && !data.loaded
+            }
         val files = MODULE_FOLDER.getFile().listFiles()
             ?.filter { it.exists() }
             ?.filter { it.isFile }
             ?.filter { it.extension == "jar" }
-            ?.filter { file -> moduleHandler.getCachedDescriptions().any { it.cachedFile == file} }
+            ?.filter { file -> moduleHandler.getCachedDescriptions().none { it.cachedFile == file} }
+            ?.filter { file ->
+                val description = kotlin.runCatching {
+                    moduleHandler.loadDescription(file)
+                }
+                if (description.isFailure) return@filter true
+                moduleHandler.getCachedDescriptions().none { it.id == description.getOrNull()!!.id }
+            }
             ?: emptyList()
         val results = descriptions.map { it.id } + files.map { it.name }
         return results.toTypedArray()
