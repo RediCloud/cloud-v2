83640e3cf51437f55589285ab1d05f6c51288c06
Suqatri <paul@suqatri.net>
2023-07-21 22:20:27 +0200

    utils functions...

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -4,8 +4,8 @@ repositories {
     mavenCentral()
 }
 
-
 dependencies {
+    compileOnly(project(":logging"))
     dependency("com.google.code.gson:gson:${Versions.gson}")
     testRuntimeOnly("com.google.code.gson:gson:${Versions.gson}")
     implementation("com.google.code.gson:gson:${Versions.gson}")
