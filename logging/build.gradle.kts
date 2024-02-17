9ace5dceb73aa2fc830728538d379fff45148875
Suqatri <paul@suqatri.net>
2024-02-18 00:00:45 +0100

    fix: minestom screen logging

--- logging/build.gradle.kts
+++ logging/build.gradle.kts
@@ -1,3 +1,8 @@
 group = "dev.redicloud"
 
 val publishToRepository by extra(true)
+
+dependencies {
+    dependency("ch.qos.logback:logback-core:1.4.12")
+    dependency("ch.qos.logback:logback-classic:1.4.12")
+}
