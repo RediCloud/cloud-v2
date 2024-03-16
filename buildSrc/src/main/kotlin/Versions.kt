b53187dd8169d6eecd070e0c3ed85b6997180cb1
Suqatri <paul@suqatri.net>
2024-03-16 14:40:20 +0100

    update kotlinx-coroutines to 1.8.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val gson = "2.10.1"
     const val libloader = "1.6.7"
     const val libloaderBootstrap = "1.6.7"
-    const val kotlinxCoroutines = "1.7.1"
+    const val kotlinxCoroutines = "1.8.0"
     const val redisson = "3.27.1"
     const val slf4j = "2.0.7"
     const val khttp = "0.1.0"
