859b9714467a03be4cf0404bff7aa0a3cfcd21fc
Suqatri <paul@suqatri.net>
2024-03-16 14:40:56 +0100

    update slf4j to 2.0.12

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val libloaderBootstrap = "1.6.7"
     const val kotlinxCoroutines = "1.8.0"
     const val redisson = "3.27.1"
-    const val slf4j = "2.0.7"
+    const val slf4j = "2.0.12"
     const val khttp = "0.1.0"
     const val minecraftVersion = "1.20.1-R0.1-SNAPSHOT"
     const val netty = "4.1.107.Final" //Check compatibility with redission
