14ac9c5e6fa9fa2e5b81d64b5ac91bca7b7044dd
Suqatri <paul@suqatri.net>
2024-03-25 21:43:12 +0100

    update example module

--- examples/example-module/src/main/kotlin/dev/redicloud/example/ExampleModule.kt
+++ examples/example-module/src/main/kotlin/dev/redicloud/example/ExampleModule.kt
@@ -15,9 +15,14 @@ class ExampleModule @Inject constructor(
     @Named("service") val logger: Logger
 ) : CloudModule(), CloudInjectable {
 
+    val config = getStorage("config")
+
     @ModuleTask(ModuleLifeCycle.LOAD, ModuleTaskOrder.EARLY)
-    fun early(@Named("this") serviceId: ServiceId) {
-        logger.info("Early task: ${serviceId.toName()}")
+    suspend fun load(@Named("this") serviceId: ServiceId) {
+        if (config.getOrDefault("first-load", { true }, Boolean::class.java)) {
+            logger.info("First load task: ${serviceId.toName()}")
+            config.set("first-load", false)
+        }
     }
 
     @ModuleTask(ModuleLifeCycle.UNLOAD, ModuleTaskOrder.FIRST)
@@ -29,6 +34,5 @@ class ExampleModule @Inject constructor(
     fun earlyUnload(@Named("this") serviceId: ServiceId) {
         logger.info("Last unload task: ${serviceId.toName()}")
     }
-
 
 }
