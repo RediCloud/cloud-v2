90f3d979a9369be4670b3e824d57e45ff2b040f7
Suqatri <paul@suqatri.net>
2024-04-07 20:42:04 +0200

    fix: building

--- test-framework/build.gradle.kts
+++ test-framework/build.gradle.kts
@@ -12,5 +12,10 @@ dependencies {
     implementation(BuildDependencies.DOCKER_TEST_CONTAINERS)
     implementation(project(":utils"))
     implementation(project(":apis:base-api"))
-    implementation(BuildDependencies.CLOUD_LIBLOADER_BOOTSTRAP)
+
+    testImplementation(BuildDependencies.LOGBACK_CORE)
+    testImplementation(BuildDependencies.LOGBACK_CLASSIC)
+    testImplementation(BuildDependencies.DOCKER_TEST_CONTAINERS)
+    testImplementation(project(":utils"))
+    testImplementation(project(":apis:base-api"))
 }
