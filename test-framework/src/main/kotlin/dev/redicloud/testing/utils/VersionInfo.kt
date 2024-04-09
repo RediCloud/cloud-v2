a706b94b7eaba39af114afeec9b638f3eafad9e4
Suqatri <paul@suqatri.net>
2024-04-09 20:24:29 +0200

    fix: build not found

--- test-framework/src/main/kotlin/dev/redicloud/testing/utils/VersionInfo.kt
+++ test-framework/src/main/kotlin/dev/redicloud/testing/utils/VersionInfo.kt
@@ -2,10 +2,10 @@ package dev.redicloud.testing.utils
 
 data class VersionInfo(
     var branch: String,
-    var build: String
+    var build: String = "latest"
 ) {
     companion object {
-        val LATEST_STABLE = VersionInfo("master", "lastest")
-        val LATEST_DEVELOPMENT = VersionInfo("dev", "lastest")
+        val LATEST_STABLE = VersionInfo("master", "latest")
+        val LATEST_DEVELOPMENT = VersionInfo("dev", "latest")
     }
 }
