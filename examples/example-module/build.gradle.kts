324fa32efbb65e6df453d15a626ecbfaec6bd76a
Suqatri <paul@suqatri.net>
2024-04-09 22:07:28 +0200

    updated example-module/plugin build.gradle.kts

--- examples/example-module/build.gradle.kts
+++ examples/example-module/build.gradle.kts
@@ -1,8 +1,17 @@
 group = "dev.redicloud.modules"
 
 repositories {
-    // Repo for redicloud artifacts
-    maven("https://repo.redicloud.dev/releases")
+    /*
+    Github redicloud packages (more information at https://docs.redicloud.dev/development/repository):
+
+    maven("https://maven.pkg.github.com/RediCloud/cloud-v2") {
+        credentials {
+            username = project.findProperty("github_username") as String? ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("github_token") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+     */
+
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
 }
 
@@ -10,8 +19,17 @@ dependencies {
     // Internal usage, ignore it
     compileOnly(project(":apis:base-api"))
 
-    // External usage, use it
-    // compileOnly("dev.redicloud:api:${Versions.cloud}")
+    /*
+    Use the code below to add the cloud api (more information at https://docs.redicloud.dev/development/repository):
+
+    compileOnly("dev.redicloud.api:base-api:<cloud-version>")
+
+    If this should be a node module, you can add also the node-api:
+    compileOnly("dev.redicloud.api:node-api:<cloud-version>")
+     */
 
     compileOnly(BuildDependencies.SPIGOT_API)
+
+    // Internal usage, ignore it! You don't need to add this
+    compileOnly(project(":apis:base-api"))
 }
--- examples/example-plugin/build.gradle.kts
+++ examples/example-plugin/build.gradle.kts
@@ -1,17 +1,30 @@
 group = "dev.redicloud.example.plugin"
 
 repositories {
-    // Repo for redicloud artifacts
-    maven("https://repo.redicloud.dev/releases")
+    /*
+    Github redicloud packages:
+
+    maven("https://maven.pkg.github.com/RediCloud/cloud-v2") {
+        credentials {
+            username = project.findProperty("github_username") as String? ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("github_token") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+     */
+
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
 }
 
 dependencies {
-    // Internal usage, ignore it
-    compileOnly(project(":apis:base-api"))
 
-    // External usage, use it
-    // compileOnly("dev.redicloud:api:<cloud-version>")
+    /*
+    Use the code below to add the cloud api:
+
+    compileOnly("dev.redicloud.api:base-api:<cloud-version>")
+     */
 
     compileOnly(BuildDependencies.SPIGOT_API)
+
+    // Internal usage, ignore it! You don't need to add this
+    compileOnly(project(":apis:base-api"))
 }
