80dc8f0f5cb2e96588c3b1e7c794ff824ec648d3
Suqatri <44299323+Suqatri@users.noreply.github.com>
2024-07-09 21:51:32 +0200

    removed test repo

--- apis/base-api/build.gradle.kts
+++ apis/base-api/build.gradle.kts
@@ -12,21 +12,3 @@ dependencies {
     compileOnly(project(":utils"))
     compileOnly(project(":logging"))
 }
-
-publishing {
-    repositories {
-        maven {
-            name = "redicloud"
-            url = URI("https://repo.redicloud.dev/snapshots/")
-            credentials(PasswordCredentials::class.java) {
-                username = findProperty("REDI_CLOUD_REPO_USERNAME") as String?
-                    ?: System.getenv("REDI_CLOUD_REPO_USERNAME")
-                password = findProperty("REDI_CLOUD_REPO_PASSWORD") as String?
-                    ?: System.getenv("REDI_CLOUD_REPO_PASSWORD")
-            }
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-    }
-}
